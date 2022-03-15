4b65b8babbcdf0f66f8f76357c8d280a23dd787a
Greden Night <greden.ua@gmail.com>
2022-03-15 21:19:06 +0200

    Добавьте файлы проекта.

---
+++ KUBIZATOR/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace KUBIZATOR
+{
+    partial class Window
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Window));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(157, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(627, 426);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(139, 426);
+            this.panel1.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(3, 3);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(133, 24);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Window";
+            this.Text = "OBJ to Minecraft schematics v0.0.1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ KUBIZATOR/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace KUBIZATOR
+{
+    public partial class Window : Form
+    {
+        public Window()
+        {
+            InitializeComponent();
+            string path = "C:\\Users\\grede\\Downloads\\untitled.obj";
+            MeshContainer meshContainer = new MeshContainer();
+            meshContainer.LoadModel(path);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// контейнер точек в формате (x,y,z)
+        /// </summary>
+        public class Point3D
+        {
+            public double X { get; set; }
+            public double Y { get; set; }
+            public double Z { get; set; }
+
+            public Point3D(double x,double y, double z)
+            {
+                X = x;
+                Y = y;
+                Z = z;
+            }
+        }
+
+        /// <summary>
+        /// класс для работы с OBJ файлами
+        /// </summary>
+        public class MeshContainer
+        {
+            /// <summary>
+            /// массив точек
+            /// </summary>
+            public List<Point3D> Verts = new List<Point3D>();
+            /// <summary>
+            /// массив полигонов, состояших из 3 вершин
+            /// </summary>
+            public List<Point3D> Polys = new List<Point3D>();
+            public int PolysDimension(string path)
+            {
+                return 0;
+            }
+
+            /// <summary>
+            /// открывает указанный obj файл и загружает его в массивы
+            /// </summary>
+            /// <param name="Path">Путь к obj файлу</param>
+            public void LoadModel(string Path)
+            {
+                switch
+                Verts.Clear(); Polys.Clear();
+                string[] Lines = File.ReadAllLines(Path);
+                foreach (string Line in Lines)
+                {
+                    //если строка начинается с v, то записываем новую вершину в массив
+                    if (Line.ToLower().StartsWith("v"))
+                    {
+                        var vx =Line.Split(' ').Skip(1).Select(v=>Double.Parse(v.Replace(".",","))).ToArray();
+                        Verts.Add(new Point3D(vx[0],vx[1],vx[2]));
+                    }
+                    //если строка описывает полигон
+                    if (Line.ToLower().StartsWith("f"))
+                    {
+                     
+                        var vx = Line.Split(' ').Skip(1).Select(v => Double.Parse(v.Replace(".", ","))).ToArray();
+
+                    }
+
+
+                }
+            }
+        }
+    }
+}
